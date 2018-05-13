651bb13ecc886759aa628895e21db99690d5b410
FreyYa <john6750@naver.com>
2018-05-13 18:00:13 +0900

    add enums

---
+++ RawModels/SellType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinMaster.FreyYa.Ticker.RawModels
+{
+	public enum SellType
+	{
+		None,
+		Half,
+		TwoThirds,
+		Total,
+	}
+}
---
+++ RawModels/TradeType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinMaster.FreyYa.Ticker.RawModels
+{
+	public enum TradeType
+	{
+		/// <summary>
+		/// 구매
+		/// </summary>
+		Bid,
+		/// <summary>
+		/// 판매
+		/// </summary>
+		Ask,
+	}
+}
